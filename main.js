3ee16ae6b2611babe99205d2d9ee7e962f7dce36
LewZhiYang <circle1329@gmail.com>
2022-01-25 09:58:33 +0800

    added listeners. bad code tho

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
 
 
-// document.getElementsByClassName('button').forEach(element => {
-//     console.log(element.textContent)
-// });
-x = document.getElementsByClassName('button')[1].textContent
-console.log(x)
+number1 = document.getElementById('1')
+number2 = document.getElementById('2')
+number3 = document.getElementById('3')
+number4 = document.getElementById('4')
+number5 = document.getElementById('5')
+number6 = document.getElementById('6')
+number7 = document.getElementById('7')
+number8 = document.getElementById('8')
+number9 = document.getElementById('9')
+add = document.getElementById('+')
+minus = document.getElementById('-')
+multiply = document.getElementById('*')
+divide = document.getElementById('/')
+c = document.getElementById('C')
+equal = document.getElementById('=')
+operationDisplay = document.getElementById('operationDisplay')
+result = document.getElementById('result')
+
+document.getElementById('0').addEventListener('click', buttonPressed('0'))
+number1.addEventListener('click', buttonPressed('1'))
+number2.addEventListener('click', buttonPressed('2'))
+number3.addEventListener('click', buttonPressed('3'))
+number4.addEventListener('click', buttonPressed('4'))
+number5.addEventListener('click', buttonPressed('5'))
+number6.addEventListener('click', buttonPressed('6'))
+number7.addEventListener('click', buttonPressed('7'))
+number8.addEventListener('click', buttonPressed('8'))
+number9.addEventListener('click', buttonPressed('9'))
+add.addEventListener('click', buttonPressed('+'))
+minus.addEventListener('click', buttonPressed('-'))
+multiply.addEventListener('click', buttonPressed('*'))
+divide.addEventListener('click', buttonPressed('/'))
+c.addEventListener('click', buttonPressed('c'))
+equal.addEventListener('click', buttonPressed('='))
+
+function buttonPressed(input) {
+    if (input == '=') {
+        evaluate()
+    } else if (input = 'c') {
+        cancel()
+    } else {
+        operationDisplay.textContent += input
+    }
+}
+
+function evaluate() {
+    try {
+        result = eval(operationDisplay.textContent)
+    } catch (error) {
+        
+    }
+}
+
+function cancel() {
+    result.textContent = ''
+    operationDisplay.textContent = ''
+}
