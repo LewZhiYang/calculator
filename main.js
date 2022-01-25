6af9d6018c59d1bdb974d70a5c438fc40d118055
LewZhiYang <circle1329@gmail.com>
2022-01-25 13:08:20 +0800

    working now

--- main.js
+++ main.js
@@ -1,59 +1,34 @@
 
+const equality = 14
+const cancel = 12
 
-number1 = document.getElementById('1')
-number2 = document.getElementById('2')
-number3 = document.getElementById('3')
-number4 = document.getElementById('4')
-number5 = document.getElementById('5')
-number6 = document.getElementById('6')
-number7 = document.getElementById('7')
-number8 = document.getElementById('8')
-number9 = document.getElementById('9')
-add = document.getElementById('+')
-minus = document.getElementById('-')
-multiply = document.getElementById('*')
-divide = document.getElementById('/')
-c = document.getElementById('C')
-equal = document.getElementById('=')
-operationDisplay = document.getElementById('operationDisplay')
-result = document.getElementById('result')
 
-document.getElementById('0').addEventListener('click', buttonPressed('0'))
-number1.addEventListener('click', buttonPressed('1'))
-number2.addEventListener('click', buttonPressed('2'))
-number3.addEventListener('click', buttonPressed('3'))
-number4.addEventListener('click', buttonPressed('4'))
-number5.addEventListener('click', buttonPressed('5'))
-number6.addEventListener('click', buttonPressed('6'))
-number7.addEventListener('click', buttonPressed('7'))
-number8.addEventListener('click', buttonPressed('8'))
-number9.addEventListener('click', buttonPressed('9'))
-add.addEventListener('click', buttonPressed('+'))
-minus.addEventListener('click', buttonPressed('-'))
-multiply.addEventListener('click', buttonPressed('*'))
-divide.addEventListener('click', buttonPressed('/'))
-c.addEventListener('click', buttonPressed('c'))
-equal.addEventListener('click', buttonPressed('='))
 
-function buttonPressed(input) {
-    if (input == '=') {
-        evaluate()
-    } else if (input = 'c') {
-        cancel()
+for (let i=0; i<16; i++) {
+    if (i == equality) {
+        document.getElementById(i).addEventListener('click', evaluate)
+    } else if (i == cancel) {
+        document.getElementById(i).addEventListener('click', reset)
     } else {
-        operationDisplay.textContent += input
+        document.getElementById(i).addEventListener('click', function() {
+            document.getElementById('operationDisplay').textContent += document.getElementById(i).textContent
+        })
     }
 }
 
 function evaluate() {
     try {
-        result = eval(operationDisplay.textContent)
+        console.log('gets here')
+        document.getElementById('result').textContent = eval(document.getElementById('operationDisplay').textContent)
+        
+        console.log('gets here2')
     } catch (error) {
         
+        console.log('gets here3')
     }
 }
 
-function cancel() {
-    result.textContent = ''
-    operationDisplay.textContent = ''
+function reset() {
+    document.getElementById('result').textContent = ''
+    document.getElementById('operationDisplay').textContent = ''
 }
