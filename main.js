6112c7667f6172743bc8ce7f2da6985a1f155634
LewZhiYang <circle1329@gmail.com>
2022-01-25 08:34:25 +0800

    figuring out how to make js take on buttons

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+// document.getElementsByClassName('button').forEach(element => {
+//     console.log(element.textContent)
+// });
+x = document.getElementsByClassName('button')[1].textContent
+console.log(x)
